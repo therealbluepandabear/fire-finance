95fc90308bb1f734a8d1f823dd8736bf9dbdc476
therealbluepandabear <therealbluepandabear@protonmail.com>
2023-04-20 00:54:32 +1200

    Update retirement indicator on graph.

--- src/components/retirement-calculator/RetirementCalculatorChart.tsx
+++ src/components/retirement-calculator/RetirementCalculatorChart.tsx
@@ -1,5 +1,5 @@
 import { Box, Text, Flex } from '@chakra-ui/react'
-import { MdFace, MdAccountBalanceWallet, MdCalendarMonth } from 'react-icons/md'
+import { MdFace, MdAccountBalanceWallet, MdCalendarMonth, MdFlag } from 'react-icons/md'
 import { 
     AreaChart, 
     Area, 
@@ -10,10 +10,10 @@ import {
     TooltipProps, 
     Tooltip, 
     ReferenceDot, 
-    ReferenceLine 
 } from 'recharts'
 import { currency } from '../../utils'
 import { RetirementCalculatorOutputs } from '../../models/Calculator'
+import { useEffect, useState } from 'react'
 
 function ChartTooltip({ active, payload, label }: TooltipProps<number, number>): JSX.Element | null {
     if (active && payload && payload[0]) {
@@ -68,58 +68,87 @@ interface RetirementCalculatorProps {
 export default function RetirementCalculatorChart(props: RetirementCalculatorProps): JSX.Element {
     const data = props.outputs.data
 
+    const [cx, setCx] = useState(0)
+    const [cy, setCy] = useState(0)
+
     function tickFormatter(value: number): string {
         return currency(value, true)
     }
 
-    const pointOfRetirement: { x: number, y: number } = {
+    const pointOfRetirement = {
         x: props.outputs.retirementAge!,
         y: props.outputs.data.filter((value) => value.age === props.outputs.retirementAge)[0].networth
     }
 
-    return (
-        <ResponsiveContainer>
-            <AreaChart data={data}>
-                <defs>
-                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="5%" stopColor="#00d450" stopOpacity={0.8}/>
-                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0.3}/>
-                    </linearGradient>
-                </defs>
-                <Area 
-                    fill="url(#colorPv)" 
-                    dataKey="networth" 
-                    stroke="#82ca9d" 
-                    strokeWidth={4} 
-                />
-                <CartesianGrid stroke="lightgray" strokeDasharray="5 5" vertical={false} />
-                <XAxis 
-                    dataKey="age" 
-                    axisLine={false} 
-                    interval={Math.floor(data.length / 26)}
-                />
-                <YAxis 
-                    tickCount={12} 
-                    tickFormatter={tickFormatter} 
-                    axisLine={false}
-                    tickLine={false}
-                />
-                <Tooltip
-                    wrapperStyle={{ outline: "none" }} 
-                    content={<ChartTooltip />}
-                />
+    // We have to use a bad hack to get the cx and cy values, this includes creating a nested component, which isn't a good
+    // practice but I don't have much choice in this scenario, it's very (hackish) but it is what is lol
+    function CustomReferenceDot(props: { cx: number, cy: number }) {
+        useEffect(() => {
+            setCx(props.cx)
+            setCy(props.cy)
+        }, [props.cx, props.cy])
+    
+        return <></>
+    }
 
-                <ReferenceLine 
-                    y={pointOfRetirement.y} 
-                    stroke="red"
-                    strokeWidth={0.5}
-                />
-                <ReferenceDot 
-                    r={10} 
-                    x={pointOfRetirement.x} 
-                    y={pointOfRetirement.y} 
-                />
-            </AreaChart>
-        </ResponsiveContainer>
+    return (  
+        <Box width="100%" height="100%" position="relative">
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart data={data}>
+                    <defs>
+                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#00d450" stopOpacity={0.8}/>
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0.3}/>
+                        </linearGradient>
+                    </defs>
+                    <Area 
+                        fill="url(#colorPv)" 
+                        dataKey="networth" 
+                        stroke="#82ca9d" 
+                        strokeWidth={4} 
+                    />
+                    <CartesianGrid stroke="lightgray" strokeDasharray="5 5" vertical={false} />
+                    <XAxis 
+                        dataKey="age" 
+                        axisLine={false} 
+                        interval={Math.floor(data.length / 26)}
+                    />
+                    <YAxis 
+                        tickCount={12} 
+                        tickFormatter={tickFormatter} 
+                        axisLine={false}
+                        tickLine={false}
+                    />
+                    <Tooltip
+                        wrapperStyle={{ outline: "none", zIndex: 999 }} 
+                        content={ChartTooltip}
+                    />
+
+                    <ReferenceDot 
+                        shape={CustomReferenceDot}
+                        r={10} 
+                        x={pointOfRetirement.x} 
+                        y={pointOfRetirement.y} 
+                    />
+                </AreaChart>
+            </ResponsiveContainer>
+
+            <Box 
+                width="20px" 
+                height="20px" 
+                borderRadius="30px" 
+                background="lightblue" 
+                position="absolute"
+                left={cx - 10}
+                top={(cy - 10) - 60}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                transform="scale(2)"
+                pointerEvents="none"
+            >
+                <MdFlag />
+            </Box>
+        </Box>
     )
 }
