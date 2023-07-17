d59b40c7f862f4c00a445d03d2880e901cd509b0
therealbluepandabear <therealbluepandabear@protonmail.com>
2023-07-17 21:19:23 +1200

    Remove dead code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import Dashboard from './components/dashboard/Dashboard'
-import RetirementCalculator from './components/retirement-calculator/RetirementCalculator'
 
 function App() {
     return (
         <Dashboard user={ { id: 0, email: 'todoescode@gmail.com', password: 'DFA2532423' } } />
-        // <RetirementCalculator />
     )
 }
 
